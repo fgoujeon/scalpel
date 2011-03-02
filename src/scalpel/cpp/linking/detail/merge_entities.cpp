33b718989e066c7cd51db0dd3ec9486474ba9c6f
Florian Goujeon <flo.goujeon@orange.fr>
2011-03-02 04:45:03 +0100

    Linking of simple function parameter list OK for simple types (array, fundamental_type, cv_qualified_type, pointer and reference)

--- src/scalpel/cpp/linking/detail/merge_entities.cpp
+++ src/scalpel/cpp/linking/detail/merge_entities.cpp
@@ -27,16 +27,25 @@ using namespace semantic_entities;
 
 namespace
 {
-	std::unique_ptr<semantic_entities::namespace_>
+	std::unique_ptr<namespace_>
 	create_namespace(const linking_tree::namespace_& tree, const std::string& namespace_name);
 
-	std::unique_ptr<semantic_entities::simple_function>
+	std::unique_ptr<simple_function>
 	create_simple_function(const simple_function& entity);
+
+	function_parameter_list
+	create_function_parameter_list(const function_parameter_list& entity);
+
+	std::unique_ptr<function_parameter>
+	create_function_parameter(const function_parameter& entity);
+
+	type_variant
+	create_type(const type_variant& entity);
 }
 
 namespace
 {
-	std::unique_ptr<semantic_entities::namespace_>
+	std::unique_ptr<namespace_>
 	create_namespace(const linking_tree::namespace_& tree, const std::string& namespace_name)
 	{
 		std::unique_ptr<namespace_> new_namespace(new namespace_(namespace_name));
@@ -74,16 +83,16 @@ namespace
 		return new_namespace;
 	}
 
-	std::unique_ptr<semantic_entities::simple_function>
+	std::unique_ptr<simple_function>
 	create_simple_function(const simple_function& entity)
 	{
-		std::unique_ptr<semantic_entities::simple_function> new_entity
+		std::unique_ptr<simple_function> new_entity
 		(
-			new semantic_entities::simple_function
+			new simple_function
 			(
 				entity.name(),
 				entity.return_type(),
-				function_parameter_list(), //entity.parameters(),
+				create_function_parameter_list(entity.parameters()),
 				entity.variadic(),
 				entity.is_static(),
 				entity.is_inline()
@@ -94,6 +103,114 @@ namespace
 			new_entity->body(std::unique_ptr<statement_block>(new statement_block()));
 
 		return new_entity;
+	}
+
+	function_parameter_list
+	create_function_parameter_list(const function_parameter_list& entity)
+	{
+		function_parameter_list new_entity;
+
+		for(auto i = entity.begin(); i != entity.end(); ++i)
+		{
+			new_entity.push_back(create_function_parameter(*i));
+		}
+
+		return new_entity;
+	}
+
+	std::unique_ptr<function_parameter>
+	create_function_parameter(const function_parameter& entity)
+	{
+		return std::unique_ptr<function_parameter>(new function_parameter(create_type(entity.type()), entity.name()));
+	}
+
+
+
+	struct create_type_visitor_struct: utility::static_visitor<type_variant>
+	{
+		type_variant
+		operator()(const array& type) const
+		{
+			return array(type.size(), create_type(type.qualified_type()));
+		}
+
+		type_variant
+		operator()(const fundamental_type& type) const
+		{
+			return type;
+		}
+
+		type_variant
+		operator()(const function_type& type) const
+		{
+			return function_type
+			(
+				create_type(type.return_type()),
+				std::vector<type_variant>(), //TODO
+				type.variadic(),
+				type.const_qualified(),
+				type.volatile_qualified()
+			);
+		}
+
+		type_variant
+		operator()(const class_* type) const
+		{
+			return type; //TODO
+		}
+
+		type_variant
+		operator()(const member_class* type) const
+		{
+			return type; //TODO
+		}
+
+		type_variant
+		operator()(const cv_qualified_type& type) const
+		{
+			return cv_qualified_type(create_type(type.qualified_type()), type.qualification());
+		}
+
+		type_variant
+		operator()(const enum_* type) const
+		{
+			return type; //TODO
+		}
+
+		type_variant
+		operator()(const member_enum* type) const
+		{
+			return type; //TODO
+		}
+
+		type_variant
+		operator()(const pointer& type) const
+		{
+			return pointer(create_type(type.qualified_type()));
+		}
+
+		type_variant
+		operator()(const pointer_to_member& type) const
+		{
+			return pointer_to_member
+			(
+				create_type(type.qualified_type()),
+				pointer_to_member::parent_class_t(static_cast<class_*>(0)) //TODO
+			);
+		}
+
+		type_variant
+		operator()(const reference& type) const
+		{
+			return reference(create_type(type.qualified_type()));
+		}
+	};
+
+	type_variant
+	create_type(const type_variant& entity)
+	{
+		create_type_visitor_struct visitor;
+		return utility::apply_visitor(visitor, entity);
 	}
 }
 
