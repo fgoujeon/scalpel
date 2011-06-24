be47a67c907d781011d8b2f2e9dc363db6f8200e
Florian Goujeon <flo.goujeon@orange.fr>
2011-06-24 10:42:23 +0200

    New test case for expression semantic analysis

---
+++ test/testfiles/semantic_analysis/expressions.cpp
@@ -0,0 +1,24 @@
+bool b1 = 7 < 9;
+bool b2 = 7 <= 9;
+bool b3 = 7 > 9;
+bool b4 = 7 >= 9;
+bool b5 = 7 == 9;
+bool b6 = 7 != 9;
+
+bool b7 = true && false;
+bool b8 = true || false;
+
+int i01 = 7 * 9;
+int i02 = 7 / 9;
+int i03 = 7 % 9;
+int i04 = 7 + 9;
+int i05 = 7 - 9;
+int i06 = 7 << 2;
+int i07 = 7 >> 2;
+
+int i08 = 7 & 9;
+int i09 = 7 ^ 9;
+int i10 = 7 | 9;
+
+int i11 = true ? 8 : 6;
+
